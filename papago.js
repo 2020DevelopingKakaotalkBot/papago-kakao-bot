40019b9c09eb4e62dfb850b9cd3498308bb39c88
delta-kor <deltakbot1@gmail.com>
2020-08-22 15:42:35 +0900

    Fix spacing

--- papago.js
+++ papago.js
@@ -7,15 +7,21 @@ const Util = {};
 Util.getTime = function() { return new Date().getTime() };
 Util.toJavaString = function(string) { return new java.lang.String(string) };
 Util.toHmacMD5 = function(message, keyString) {
+
     message = Util.toJavaString(message);
     keyString = Util.toJavaString(keyString);
+
     const key = new SecretKeySpec(keyString.getBytes('utf-8'), 'HmacMD5');
     const mac = Mac.getInstance('HmacMD5');
+
     mac.init(key);
     const bytes = mac.doFinal(message.getBytes('ASCII'));
+
     const encoded = Base64.getEncoder().encode(bytes);
     const base64 = new java.lang.String(encoded);
+
     return base64;
+
 };
 
 const URL = {
@@ -29,6 +35,7 @@ const Papago = (function() {
     }
 
     function generateKey(url) {
+
         const time = Util.getTime();
         let tower = Util.getTime();
 
@@ -44,29 +51,40 @@ const Papago = (function() {
         const key = 'PPG ' + chip + ':' + Util.toHmacMD5(chip + '\n' + url + '\n' + time, version);
 
         return { time: time, key: key };
+
     }
 
     Papago.prototype.detectLanguage = function(string) {
+
         if(typeof string !== 'string') {
             throw new TypeError('Parameter type must be string');
         }
-        const connection = Jsoup.connect(URL.DETECT);
+
         const keySet = generateKey(URL.DETECT);
+        const connection = Jsoup.connect(URL.DETECT);
+
         connection.method(Connection.Method.POST);
         connection.header('Authorization', keySet.key);
         connection.header('Timestamp', keySet.time);
         connection.data('query', string);
+
         connection.ignoreContentType(true);
         connection.ignoreHttpErrors(true);
+
         const response = connection.execute();
+
         if(response.statusCode() !== 200) {
             throw new Error('Language detection failed');
         }
+
         const body = response.body();
         const data = JSON.parse(body);
         let language = data.langCode;
+
         if(language === 'unk') language = null;
+
         return language;
+
     }
 
     return Papago;
